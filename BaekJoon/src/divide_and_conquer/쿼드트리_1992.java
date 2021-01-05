68d7b7eac30302145ea7de232777bada7a24d67d
thalals <thalsal@naver.com>
2021-01-05 23:41:09 +0900

    first commit

---
+++ BaekJoon/src/divide_and_conquer/쿼드트리_1992.java
@@ -0,0 +1,15 @@
+/*
+ * 백준 1992번 실버 1
+ * 쿼드트리, 분할 정복
+ */
+
+package divide_and_conquer;
+
+public class 쿼드트리_1992 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
