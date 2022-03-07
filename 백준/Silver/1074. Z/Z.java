ce11436dcf26a4824c011ed69710b79faca7a805
Park Hyeongmin <thalsal@naver.com>
2022-03-07 17:34:57 +0900

    [Silver I] Title: Z, Time: 168 ms, Memory: 16360 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, r, c, cnt;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String temp[] = br.readLine().split(" ");
+
+        n = Integer.parseInt(temp[0]);
+        r = Integer.parseInt(temp[1]);
+        c = Integer.parseInt(temp[2]);
+
+
+        solve((int) Math.pow(2, n), 0, 0);
+
+    }
+    static void solve(int n, int y, int x) {
+        if (y == r && x == c) {
+            System.out.println(cnt);
+            System.exit(0);
+        }
+
+        if (y <= r && r < (y + n) && x <= c && c < (x + n)) {
+            int nn = n / 2;
+            solve(nn, y, x);
+            solve(nn, y, x + nn);
+            solve(nn, y + nn, x);
+            solve(nn, y + nn, x + nn);
+        } else
+            cnt += n * n;
+    }
+
+}
