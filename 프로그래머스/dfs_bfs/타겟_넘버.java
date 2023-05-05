170f0fbf26f7a008a4bfe5251271abdec4a06ddb
Hyeongmin Park <thalsal@naver.com>
2023-05-05 22:05:27 +0900

    level 2 : 타켓 넘버 (dfs)

---
+++ 프로그래머스/dfs_bfs/타겟_넘버.java
@@ -0,0 +1,27 @@
+package dfs_bfs;
+
+//dfs
+public class 타겟_넘버 {
+
+    public static void main(String[] args) {
+        //  [1, 1, 1, 1, 1] 	3
+        //  [4, 1, 2, 1]	4
+        System.out.println(solution(new int[]{1, 1, 1, 1, 1}, 3));
+    }
+    public static int solution(int[] numbers, int target) {
+        return dfs(numbers, target, 0, 0);
+    }
+
+    private static int dfs(int[] number, int target, int depth, int sum) {
+
+        if (depth == number.length) {
+            if (sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+
+        return dfs(number, target, depth + 1, sum + number[depth]) +
+            dfs(number, target, depth + 1, sum - number[depth]);
+    }
+}
